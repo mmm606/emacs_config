af54fcbd75eb4b401eea13c02152ed666b3387ab
mmm606 <mmm606@scarletmail.rutgers.edu>
2024-02-13 13:12:52 -0500

    it is glorious

---
+++ elpa/forge-20240212.1715/forge-pkg.el
@@ -0,0 +1,23 @@
+(define-package "forge" "20240212.1715" "Access Git forges from Magit."
+  '((emacs "25.1")
+    (compat "29.1.4.4")
+    (closql "20240125")
+    (dash "2.19.1")
+    (emacsql "20240124")
+    (ghub "20240101")
+    (let-alist "1.0.6")
+    (magit "20240125")
+    (markdown-mode "2.6")
+    (seq "2.24")
+    (transient "20240201")
+    (yaml "0.5.5"))
+  :commit "e7e84e7dec6614e606a2672a3cfc2268db1a6e49" :authors
+  '(("Jonas Bernoulli" . "jonas@bernoul.li"))
+  :maintainer
+  '("Jonas Bernoulli" . "jonas@bernoul.li")
+  :keywords
+  '("git" "tools" "vc")
+  :url "https://github.com/magit/forge")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
