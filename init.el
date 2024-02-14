ea0bcca12e4d9862e1a902c60682994303b38756
mmm606 <mmm606@scarletmail.rutgers.edu>
2024-02-13 19:30:37 -0500

    fixed old counsel issues

--- init.el
+++ init.el
@@ -1,1 +1,14 @@
 (org-babel-load-file (expand-file-name "config.org"))
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages
+   '(0blayout which-key vertico use-package treesit-auto rainbow-delimiters python-mode org-bullets orderless nerd-icons-completion marginalia lsp-ui lsp-pyright lsp-ivy ivy-rich helpful general forge evil-nerd-commenter evil-collection embark-consult doom-themes doom-modeline dap-mode counsel-projectile company-box)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
