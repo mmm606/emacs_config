9fd0911c8c8a4578705cba236e83bce7820c8d84
mmm606 <mmm606@scarletmail.rutgers.edu>
2024-03-25 17:30:06 -0400

    added vterm and a couple keymaps

--- init.el
+++ init.el
@@ -17,8 +17,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(doc-view-continuous t)
+ '(doc-view-resolution 500)
  '(package-selected-packages
-   '(pyvenv projectile tree-sitter-langs treesit tree-sitter merlin 0blayout which-key vertico use-package treesit-auto rainbow-delimiters python-mode org-bullets orderless nerd-icons-completion marginalia lsp-ui lsp-pyright lsp-ivy ivy-rich helpful general forge evil-nerd-commenter evil-collection embark-consult doom-themes doom-modeline dap-mode counsel-projectile company-box))
+   '(vterm pyvenv projectile tree-sitter-langs treesit tree-sitter merlin 0blayout which-key vertico use-package treesit-auto rainbow-delimiters python-mode org-bullets orderless nerd-icons-completion marginalia lsp-ui lsp-pyright lsp-ivy ivy-rich helpful general forge evil-nerd-commenter evil-collection embark-consult doom-themes doom-modeline dap-mode counsel-projectile company-box))
  '(which-key-show-transient-maps t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
